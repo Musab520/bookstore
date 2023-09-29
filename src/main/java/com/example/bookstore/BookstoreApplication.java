5e5461148f430b0741e30ea596acbfccbe0a5f38
Musab Abuasi <abuasimusab@gmail.com>
2023-09-29 23:37:41 +0300

    Adding Flyway Migrations

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,17 +1,23 @@
 package com.example.bookstore;
 
 import com.example.bookstore.application.BookstoreController;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationVersion;
 
 import java.io.IOException;
 
 public class BookstoreApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        setupFlyway();
+
         FXMLLoader fxmlLoader = new FXMLLoader(BookstoreApplication.class.getResource("bookstore-root.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
@@ -21,5 +27,27 @@ public class BookstoreApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static void setupFlyway() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:mariadb://localhost:33306/bookstore");
+        hikariConfig.setUsername("admin");
+        hikariConfig.setPassword("pass123");
+        hikariConfig.setSchema("bookstore");
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+        Flyway flyway = Flyway.configure()
+                .schemas("bookstore")
+                .locations("src/main/resources/migration/flyway/bookstore")
+                .table("flyway_schema_history")
+                .baselineOnMigrate(true)
+                .baselineVersion("0")
+                .validateMigrationNaming(true)
+                .target(MigrationVersion.LATEST)
+                .dataSource(dataSource)
+                .load();
+        flyway.repair();
+        flyway.migrate();
     }
 }
---
+++ src/main/resources/migration/flyway/bookstore/V0_1_0__create_initial_schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE book(title VARCHAR(255), author VARCHAR(255),
+publisher VARCHAR(255), `row` INT, shelf VARCHAR(255), cost DOUBLE, price DOUBLE);
