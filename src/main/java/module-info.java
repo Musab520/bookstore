be12169423000e27cd276be56bf3ba94e1bc7960
Musab Abuasi <abuasimusab@gmail.com>
2023-09-30 08:37:39 +0300

    Adding Flyway to application(feature flagged until ready to add database)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,5 +17,6 @@ module com.example.bookstore {
     exports com.example.bookstore.application;
     opens com.example.bookstore.application to javafx.fxml, org.flywaydb.core;
     opens com.example.bookstore to javafx.fxml;
+    opens migration.flyway.bookstore;
     exports com.example.bookstore;
 }
